04e5b7eb9fc4e4558cbbf41f32e1cf2d87dfabf7
cheekiang_james <tancheekiang@yahoo.com.sg>
2016-09-08 08:55:42 +0800

    Initial Commit

---
+++ UAS Command/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UAS Command
+//
+//  Created by Chee Kiang Tan on 8/9/16.
+//  Copyright © 2016 Govtech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
